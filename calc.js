5b93f8c154eca1367987f6972472b4a931a528e4
ClaraMaracaja <mrclrmaracaja@gmail.com>
2024-08-28 21:23:06 -0300

    primeiro commit

---
+++ calc.js
@@ -0,0 +1,37 @@
+let x = 20
+let y = 5
+
+let soma = x + y
+let subtração = x - y
+let multiplicação = x * y
+let divisão = x / y
+let operador = "+"
+
+console.log("A soma é: " + soma) 
+console.log("A subtração é: " + subtração)
+console.log("A multiplicação é: " + multiplicação)
+console.log("A divisão é : " + divisão)
+
+if (y == 0) {
+    console.log("Não é possivel dividir por 0")
+} else{
+    divisão = x / y}
+    switch (operador) {
+        case "+":
+            result = x + y
+            break
+        case "+":
+            result = x - y
+            break
+        case "*":
+            result = x * y
+            break
+        case "/":
+            result = x / y
+            default:
+            result = 'operador invalido'
+    }
+
+
+
+
